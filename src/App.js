dd8f5d57b9f73dd0c5be92133b975429a0e70e2d
kamel bouali <bouali.kamel@grp-sonelgaz.sng>
2020-01-20 16:26:40 +0100

    integrate a menu and content ant-d

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
-import { Layout } from 'antd';
-const { Content } = Layout;
+
+import { Layout, Menu,  Icon } from 'antd';
+const { Content} = Layout;
+
 function App() {
   return (
     <div className="App">
     <Layout className="layout">
-        <Content style={{ padding: '0 50px' }}>
-        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-      </Content>
+    <Content style={{ height:'100%',padding: '24px 24px' }}>
+      <Layout style={{ background: '#fff',  height:'100%' }}>
+          <Menu mode="horizontal" defaultSelectedKeys={['1']} style={{ textAlign: 'center'}}>
+          
+              <Menu.Item key="1"><span><Icon type="user" />About</span></Menu.Item>
+              <Menu.Item key="2"><span><Icon type="project" />Projects</span></Menu.Item>
+              <Menu.Item key="3"><span><Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" />Heart</span></Menu.Item>
+              <Menu.Item key="4"><span><Icon type="message" theme="twoTone" twoToneColor="#52c41a"/>Contact</span></Menu.Item>
+              <Menu.Item key="5"><span><Icon type="github" />Github</span></Menu.Item>
+           
+          </Menu>
+        <Content style={{ padding: '24px 24px', minHeight: 280 }}>Content</Content>
+      </Layout>
+    </Content>
     </Layout>
     </div>
   );
